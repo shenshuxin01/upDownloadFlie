07bc803cf8c378261f1254ccfc38805ebc2a4878
shenshuxin <shenshuxin@thwb.com.cn>
2024-06-18 09:35:17 +0800

    兼容mac和windows

--- updownloadfile/src/main/java/cn/shenshuxin/updownloadfile/contr/Controller.java
+++ updownloadfile/src/main/java/cn/shenshuxin/updownloadfile/contr/Controller.java
@@ -1,6 +1,7 @@
 package cn.shenshuxin.updownloadfile.contr;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,10 +12,15 @@ import java.io.IOException;
 @RestController
 public class Controller {
 
+    @Value("${ssx.up.path}")
+    private String upPath;
+
     @PostMapping(value = "/up",consumes = "multipart/form-data")
     public String  upload(@RequestParam("file") MultipartFile file) throws IOException {
         log.info("上传文件:{}", file.getOriginalFilename());
-        file.transferTo(new File("/Users/shenshuxin/Downloads/up/"+file.getOriginalFilename()));
+        File dest = new File(upPath + file.getOriginalFilename());
+        dest.getParentFile().mkdirs();
+        file.transferTo(dest);
         return "OK";
     }
 
