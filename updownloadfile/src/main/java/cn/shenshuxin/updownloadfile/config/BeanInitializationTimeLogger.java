0154819f085b7c0f8228bc0cda909a332c6b2773
shenshuxin <shenshuxin@thwb.com.cn>
2025-01-13 10:01:35 +0800

    feat: 排序

--- updownloadfile/src/main/java/cn/shenshuxin/updownloadfile/config/BeanInitializationTimeLogger.java
+++ updownloadfile/src/main/java/cn/shenshuxin/updownloadfile/config/BeanInitializationTimeLogger.java
@@ -3,16 +3,18 @@ package cn.shenshuxin.updownloadfile.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Component
-public class BeanInitializationTimeLogger implements BeanPostProcessor {
+public class BeanInitializationTimeLogger implements BeanPostProcessor, CommandLineRunner {
 
-    private final Map<String, Long> startTimeMap = new HashMap<>();
+    private final Map<String, Long> startTimeMap = new TreeMap<>();
+
+    private final Map<Long, List<String>> map = new TreeMap<>(Collections.reverseOrder());
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -22,11 +24,27 @@ public class BeanInitializationTimeLogger implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        Long startTime = startTimeMap.remove(beanName);
+        Long startTime = startTimeMap.get(beanName);
         if (startTime != null) {
             long endTime = System.currentTimeMillis();
-            log.info("Bean '{}' initialized in {} ms", beanName, endTime - startTime);
+            long c = endTime - startTime;
+            map.compute(c, (k, v) -> {
+                if (v == null) {
+                    v = new ArrayList<>();
+                }
+                v.add(beanName);
+                return v;
+            });
+
+            log.info("Bean '{}' initialized in {} ms", beanName, c);
         }
         return bean;
+    }
+
+    @Override
+    public void run(String... args) {
+        map.forEach((k, v) -> {
+            log.info("Bean initialization time: {} ms,size:{}, list:{}", k,v.size(), v);
+        });
     }
 }
